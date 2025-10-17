794cb85eb08cbe162ad1dc9b9cd41b8a08096e08
marcelojacint <marcelojacintoferreirq@gmail.com>
2025-10-17 18:27:13 -0300

    cria interface repository

---
+++ src/main/java/com/marcelodev/biblioteca/repository/BibliotecaRepository.java
@@ -0,0 +1,15 @@
+package com.marcelodev.biblioteca.repository;
+
+import com.marcelodev.biblioteca.infrastructure.model.Emprestimo;
+import com.marcelodev.biblioteca.infrastructure.model.Livro;
+import com.marcelodev.biblioteca.infrastructure.model.Usuario;
+
+import java.util.List;
+
+public interface BibliotecaRepository {
+    void salvarLivro(Livro livro);
+    void salvarUsuario(Usuario usuario);
+    List<Livro> listarLivros();
+    List<Usuario> listarUsuarios();
+    List<Emprestimo> listarEmprestimos();
+}
